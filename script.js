3ef5aa8c92bc3c2494f8323d6303e2fb130a9962
sonja-ops <sodju@hotmail.com>
2024-12-03 10:08:48 +0100

    rettet ut

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-const submit.Button = document.querySelector("#submitButton")
+const submitButton = document.querySelector("#submitButton")
 
-
+submitButton.addEventListener("click", function (event) {
+    event.preventDefault()
+    console.log("Button er clicked")
+})
